aa9a06b071f795866d7bc20e60cba24b8660710b
= <co.lorenzo@outlook.com>
2021-05-13 15:00:34 +0200

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { RecoilRoot } from 'recoil';
+import { RecoilRoot } from "recoil";
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 
 import './App.css';
 import ToDoList from "./components/ToDoList";
@@ -8,23 +9,33 @@ import AddToDo from "./components/AddToDo";
 
 function App() {
   return (
+    <Router>
     <div>
       <div className="header">
         <h1>ToDo App</h1>
+        <nav>
+          <Link to="/agenda">Home</Link>
+          <Link to="/agenda/crud">CRUD</Link>
+        </nav>
       </div>
-      <div>
-        <RecoilRoot>
-          <div className="main">
-            <div className="sidebar">
-              <AddToDo />
+      <Switch>
+        <Route exact path="/agenda/">
+        <div>
+          <RecoilRoot>
+            <div className="main">
+              <div className="sidebar">
+                <AddToDo />
+              </div>
+              <div className="list">
+                <ToDoList />
+              </div>
             </div>
-            <div className="list">
-              <ToDoList />
-            </div>
-          </div>
-        </RecoilRoot>
-      </div>
+          </RecoilRoot>
+        </div>
+        </Route>
+      </Switch>
     </div>
+    </Router>
   );
 };
 
